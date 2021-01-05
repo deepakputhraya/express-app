b2a5bc15d59c2a0be9dc40e510c9dd5dfeb5ccae
deepakputhraya <deepakputhraya@gmail.com>
2021-01-06 05:19:47 +0530

    Add loggers

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
+
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+const expressLogger = expressPino({ logger });
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(expressLogger);
 
 app.get('/ping', (req, res) => {
     res.send({
@@ -9,11 +16,12 @@ app.get('/ping', (req, res) => {
 })
 
 app.get('/headers', (req, res) => {
+    logger.info({headers: req.headers});
     res.send({
         headers : req.headers
     })
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    logger.info('Server running on port %d', port);
 })
