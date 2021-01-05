6b8ded2dd226c518fac1a782467491fe011bfb6d
deepakputhraya <deepakputhraya@gmail.com>
2021-01-05 20:04:37 +0530

    Simple express app

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/ping', (req, res) => {
+    res.send({
+        message : "pong!"
+    })
+})
+
+app.get('/headers', (req, res) => {
+    res.send({
+        headers : req.headers
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
